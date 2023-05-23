1eba2cd99de0f1923bd304a7c560ae038d111f96
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-05-23 20:06:01 +0400

    doc: remove files

---
+++ src/views/TheLandingPage.vue
@@ -0,0 +1,7 @@
+<script setup>
+import TheHeader from '../components/TheHeader.vue'
+</script>
+
+<template>
+  <main>landing page</main>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '1024px',
+      lg: '1920px'
+    },
     extend: {}
   },
   plugins: []
