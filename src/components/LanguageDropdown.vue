922147cf99a0665848868a9a7816be58e65a9325
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-07-06 18:49:07 +0400

    fix: locale

--- src/components/LanguageDropdown.vue
+++ src/components/LanguageDropdown.vue
@@ -1,43 +1,34 @@
 <template>
   <select
-    v-model="currentLocale"
-    class="text-white bg-gray text-sm sm:text-lg w-12"
-    @change="changeLocale(currentLocale)"
+    v-model="state.currentLocale"
+    class="text-white bg-gray text-sm sm:text-lg w-12 sm:w-14"
+    @change="changeLocale(state.currentLocale)"
   >
     <option
       v-for="locale in $i18n.availableLocales"
       :key="`locale-${locale}`"
       :value="locale"
-      :selected="currentLocale === locale"
+      :selected="state.currentLocale === locale"
     >
       {{ $t(`landing.${locale}`) }}
     </option>
   </select>
 </template>
 
-<script>
+<script setup>
 import { setLocale } from '@vee-validate/i18n'
+import { useI18n } from 'vue-i18n'
+import { reactive, onMounted } from 'vue'
+const { locale } = useI18n()
 
-export default {
-  data() {
-    return {
-      currentLocale: 'en',
-      locales: ['en', 'ka']
-    }
-  },
-  methods: {
-    changeLocale(locale) {
-      this.$i18n.locale = locale
-      localStorage.setItem('locale', locale)
-      setLocale(locale)
-      this.currentLocale = locale
-    }
-  },
+const state = reactive({
+  currentLocale: localStorage.getItem('locale') || 'en'
+})
 
-  created() {
-    this.currentLocale = localStorage.getItem('locale')
-    this.$i18n.locale = localStorage.getItem('locale')
-    setLocale(localStorage.getItem('locale'))
-  }
+const changeLocale = (newLocale) => {
+  locale.value = newLocale
+  localStorage.setItem('locale', newLocale)
+  setLocale(newLocale)
+  state.currentLocale = newLocale
 }
 </script>
